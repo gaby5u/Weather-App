4c517e86c4176a87b609e3a581b3dcf07023e1ec
gaby5u <gabrielaoloeri@gmail.com>
2025-01-19 17:13:47 +0200

    Add error handling

--- src/functions/getGeolocation.jsx
+++ src/functions/getGeolocation.jsx
@@ -1,10 +1,15 @@
 import { fetchWeather } from "../redux/WeatherSlice";
 
 const getGeolocation = (dispatch) =>
-  navigator.geolocation.getCurrentPosition((position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    dispatch(fetchWeather(`${latitude}, ${longitude}`));
-  });
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      dispatch(fetchWeather(`${latitude}, ${longitude}`));
+    },
+    (error) => {
+      dispatch(fetchWeather("Moldova"));
+    }
+  );
 
 export default getGeolocation;
